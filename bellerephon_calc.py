f85ddf4b8b09f7a1af150c7c25f8ae3583d16f53
Schwarting <meschw04@cardmail.louisville.edu>
2018-07-19 14:08:37 -0600

    a look at errors...

--- bellerephon_calc.py
+++ bellerephon_calc.py
@@ -196,10 +196,19 @@ def dehydral(x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4):
 def transition_charges(hamiltonian,vxd,vyd,vzd,x,y,z,n,protein):
     #CALCULATE THE DISPLACED ATOMS
     amplitude=(10**10)*np.sqrt(h/(8*math.pi*math.pi*nma_freq*c*nma_mass*amu))
-    xd = [[[0,0,0,0]]*n]*2
-    yd = [[[0,0,0,0]]*n]*2
-    zd = [[[0,0,0,0]]*n]*2
-    sign = [0]*n
+    xd1 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    yd1 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    zd1 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    xd2 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    yd2 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    zd2 = [[0 for _ in range(0,4)] for _ in range(0,n)]
+    xd = [xd1,xd2]
+    yd = [yd1,yd2]
+    zd = [zd1,zd2]
+    #vzd = [0 for _ in range(n)]    
+    #    yd = [[[0,0,0,0]]*n]*2
+    #zd = [[[0,0,0,0]]*n]*2
+    sign = [0 for _ in range(n)]
     dipposx = []
     dipposy = []
     dipposz = []
@@ -233,22 +242,44 @@ def transition_charges(hamiltonian,vxd,vyd,vzd,x,y,z,n,protein):
                 xd[1][i][j] = x[i][j]-(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
                 yd[1][i][j] = y[i][j]-(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
                 zd[1][i][j] = z[i][j]-(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
+#                xd[0][i][j] = x[i][j]+(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
+#                yd[0][i][j] = y[i][j]+(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
+#                zd[0][i][j] = z[i][j]+(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
+#                xd[1][i][j] = x[i][j]+(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
+#                yd[1][i][j] = y[i][j]+(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
+#                zd[1][i][j] = z[i][j]+(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
+#                if (amplitude/2)*(a_CO*vzCO+a_CN*vzCN) > 0.0001:
+#                    print (amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
+#                    print amplitude
             else:
+#                xd[0][i][j] = x[i][j]-(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
+#                yd[0][i][j] = y[i][j]-(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
+#                zd[0][i][j] = z[i][j]-(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)                
+#                xd[1][i][j] = x[i][j]-(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
+#                yd[1][i][j] = y[i][j]-(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
+#                zd[1][i][j] = z[i][j]-(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
                 xd[0][i][j] = x[i][j]-(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
                 yd[0][i][j] = y[i][j]-(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
-                zd[0][i][j] = z[i][j]-(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
-                
+                zd[0][i][j] = z[i][j]-(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)                
                 xd[1][i][j] = x[i][j]+(amplitude/2)*(a_CO*vxCO+a_CN*vxCN)
                 yd[1][i][j] = y[i][j]+(amplitude/2)*(a_CO*vyCO+a_CN*vyCN)
                 zd[1][i][j] = z[i][j]+(amplitude/2)*(a_CO*vzCO+a_CN*vzCN)
 #    CALCULATE TRANSITION DIPOLES
             for sig in range(-1,3):
                 q = q_mode[j]+sign[i]*0.5*sig*q_der[j]
-                vxd[i] = sig*(xd[0][i][j]+xd[1][i][j])*q*Debye
-                vyd[i] = sig*(yd[0][i][j]+yd[1][i][j])*q*Debye
-                vzd[i] = sig*(zd[0][i][j]+zd[1][i][j])*q*Debye
-        print [vxd[i],vyd[i],vzd[i]]
-        print (vxd[i]**2 + vyd[i]**2 + vzd[i]**2)**0.5
+#                vxd[i] = sig*(xd[0][i][j]+xd[1][i][j])*q*Debye
+#                vyd[i] = sig*(yd[0][i][j]+yd[1][i][j])*q*Debye
+#                vzd[i] = sig*(zd[0][i][j]+zd[1][i][j])*q*Debye
+                vxd[i] = sig*(xd[0][i][j]-xd[1][i][j])*q*Debye #Roeters used plus signs here.
+                vyd[i] = sig*(yd[0][i][j]-yd[1][i][j])*q*Debye #We subtracted instead by ignoring
+                vzd[i] = sig*(zd[0][i][j]-zd[1][i][j])*q*Debye #the "sign" list instantiated above.
+#                if vxd[i] != 0 and vyd[i] !=0 and vzd[i] != 0:
+#                    print vxd[i]
+#                    print vyd[i]
+#                    print vzd[i]
+#                print (vxd[i]**2 + vyd[i]**2 + vzd[i]**2)**0.5
+#        print [vxd[i],vyd[i],vzd[i]]
+#        print (vxd[i]**2 + vyd[i]**2 + vzd[i]**2)**0.5
         if (vxd[i]**2 + vyd[i]**2 + vzd[i]**2)**0.5 > 0.4:
             #print 'Transition Dipole '+str(i)+'length is too long!'
             pass
@@ -260,22 +291,29 @@ def transition_charges(hamiltonian,vxd,vyd,vzd,x,y,z,n,protein):
         vyCN = y[i][1]-y[i][3]
         vzCN = z[i][1]-z[i][3]
         len_CO = (vxCO**2 + vyCO**2 + vzCO**2)**0.5
-        print 'len_CO = '+str(len_CO)
+#        print 'len_CO = '+str(len_CO)
         dipposx.append(x[i][1]+(x[i][2]-x[i][1])*(0.868/len_CO))
         dipposy.append(y[i][1]+(y[i][2]-y[i][1])*(0.868/len_CO))
         dipposz.append(z[i][1]+(z[i][2]-z[i][1])*(0.868/len_CO))
         if len_CO>1.3:
             #print 'The CO-length on '+str(i)+' is too long!'
             pass
-    
-    
-    
+#    print x[:10]
+#    print y[:10]
+#    print z[:10]
+#    print sign
+#    print xd[:2]
+#    print yd[:2]
+#    print zd[:2]
+    dipdist2 = []
     for i in range(1,n):#Note: This is in range 1 to n, not 0 to n!
+        dipdist1 = []
         for j in range(0,n):
             if j>=i:
                 pass
             else:
                 dipdist = ((dipposx[i]-dipposx[j])**2 + (dipposy[i]-dipposy[j])**2 + (dipposz[i]-dipposz[j])**2)**0.5
+                dipdist1.append(dipdist)
                 dipvecx = dipposx[i]-dipposx[j]
                 dipvecy = dipposy[i]-dipposy[j]
                 dipvecz = dipposz[i]-dipposz[j]
@@ -288,6 +326,8 @@ def transition_charges(hamiltonian,vxd,vyd,vzd,x,y,z,n,protein):
                                         ((dipvecx*vxd[j])+(dipvecy*vyd[j])+(dipvecz*vzd[j])))/((dipdist**5)))
                 hamiltonian[i][j] = hamiltonian[i][j]*5033
                 hamiltonian[j][i] = hamiltonian[i][j] #Where the non-coupled diagonals are calculated...
+        dipdist2.append(dipdist1)
+    print dipdist2
     return hamiltonian,vxd,vyd,vzd,x,y,z,n,protein
 
 def parameterizedB3LYP(hamiltonian,x,y,z,n):
@@ -326,6 +366,11 @@ def HydrogenBond(hamiltonian,x,y,z,n,prolist): #See line 730 in template.c
                 hamiltonian[i][i]-=DonorHBshift1*(3.5-dist1)
             elif dist1<=2.6 and alpha<-np.cos(anglehydrogen/180*math.pi) and prolist[j]:
                 hamiltonian[j][j]-=ProlineHBshift1*(3.5-dist1)
+    for i in range(0,n):
+        for j in range(0,n):
+            if i == j:
+                hamiltonian[i][j] = 0
+
     return hamiltonian, x, y, z, n, prolist
                 
             
@@ -441,6 +486,7 @@ def parse_pdb_file(file_name):
 parse_pdb_file('/Users/mschwart/vsfg-bellerephon/2chb.pdb')
 
 """
+qrs = l
 
 haa_file = '/Users/mschwart/vsfg-bellerephon/sfg2/output_prepped_haa.txt'
 t = open(haa_file,'rb').read()
@@ -466,4 +512,314 @@ fig.colorbar(plotted)
 plt.title('Heatmap of the PDB Hamiltonian (abs. value)')
 plt.show()
 
+#print o
+#print l
+
+#print o
+#print ''
+#print qrs
+
+#p1 = o[0][2:25]
+#print ''
+#p2 = qrs[0][2:25]
+
+#print [i/j for (i,j) in zip(p1,p2)]
+
+
+'''
+7/19/2018
+Vance and I worked on the stuff. We got the following outputs from Roeters code.
+The first block represents the dipole distances, which are different from ours for some reason.
+The second block represents the outputs vxd, vyd, vzd, etc., see line 624 of Roeters.
+The main issue here is that the k-nearest neighbors (not the diagonals or diagonal adjacents)
+are having major issues. This is what needs to be debugged; that is, all the Hamiltonian
+values that aren't a diagonal or adjacent to a diagonal. The next block is another look at
+the various dipole distances in the system. We just gotta fix this issue then we'll be groovy.
+
+first group of the dipole distances...
+nan
+  nan
+3.050
+  nan
+4.926
+3.518
+  nan
+4.857
+4.901
+3.628
+  nan
+6.086
+4.949
+5.071
+3.353
+  nan
+8.519
+6.507
+5.044
+5.135
+3.720
+  nan
+8.418
+7.533
+5.600
+3.747
+3.838
+3.299
+  nan
+11.955
+10.887
+9.201
+7.392
+6.435
+5.233
+3.780
+  nan
+12.762
+10.873
+9.452
+8.851
+7.046
+4.496
+5.511
+3.620
+  nan
+16.045
+14.009
+12.540
+12.152
+10.327
+7.647
+8.696
+6.066
+3.329
+  nan
+18.056
+16.399
+14.209
+13.586
+12.653
+9.922
+9.873
+6.898
+6.077
+4.226
+  nan
+18.993
+17.718
+16.017
+14.466
+13.207
+11.520
+10.812
+7.079
+7.424
+5.971
+4.370
+  nan
+23.143
+21.827
+20.135
+18.624
+17.306
+15.571
+14.971
+11.240
+11.298
+9.124
+7.098
+4.163
+  nan
+297.513
+295.511
+293.364
+293.084
+291.872
+289.089
+289.356
+286.012
+284.897
+281.685
+279.510
+279.256
+275.340
+  nan
+296.708
+294.692
+292.578
+292.296
+291.048
+288.275
+288.566
+285.210
+284.072
+280.851
+278.718
+278.445
+274.523
+3.490
+  nan
+299.813
+297.814
+295.710
+295.389
+294.133
+291.388
+291.655
+288.281
+287.169
+283.952
+281.815
+281.492
+277.553
+5.711
+4.513
+  nan
+297.745
+295.767
+293.673
+293.307
+292.044
+289.331
+289.569
+286.175
+285.094
+281.882
+279.742
+279.361
+275.404
+8.413
+7.084
+4.295
+  nan
+298.949
+296.995
+294.914
+294.497
+293.226
+290.550
+290.757
+287.340
+286.293
+283.087
+280.944
+280.499
+276.523
+12.463
+11.405
+8.001
+4.339
+  nan
+302.795
+300.846
+298.749
+298.336
+297.082
+294.398
+294.597
+291.186
+290.148
+286.945
+284.783
+284.350
+280.378
+12.701
+12.379
+8.147
+6.389
+4.193
+  nan
+305.431
+303.495
+301.399
+300.963
+299.711
+297.042
+297.223
+293.803
+292.785
+289.587
+287.416
+286.957
+282.978
+15.489
+15.398
+11.039
+9.638
+6.937
+3.253
+
+
+
+
+
+x,y,z & length of dipole 1:    nan    nan    nan    nan
+Distance between C-atom of backbone and the dipole at peptide bond 1: nan
+Transition Dipole peptide bond 1: nan nan nan, Length: nan and CO vector and length: (0.000000 0.000000 0.000000, 0.000000)
+
+ x,y,z & length of dipole 2:  0.196 -0.187 -0.240  0.362
+Distance between C-atom of backbone and the dipole at peptide bond 2: 0.868000
+Transition Dipole peptide bond 2: 0.195559 -0.187031 -0.240357, Length: 0.361932 and CO vector and length: (-0.598007 1.041992 0.259998, 1.229211)
+
+ x,y,z & length of dipole 3:  0.335 -0.112  0.112  0.371
+Distance between C-atom of backbone and the dipole at peptide bond 3: 0.868000
+Transition Dipole peptide bond 3: 0.335226 -0.111812 0.112163, Length: 0.370754 and CO vector and length: (-0.871994 0.864014 -0.038002, 1.228144)
+
+ x,y,z & length of dipole 4: -0.064 -0.339  0.127  0.367
+Distance between C-atom of backbone and the dipole at peptide bond 4: 0.868000
+Transition Dipole peptide bond 4: -0.063739 -0.338550 0.127362, Length: 0.367287 and CO vector and length: (-0.222000 1.207977 0.067997, 1.230088)
+
+ x,y,z & length of dipole 5:  0.038 -0.255 -0.281  0.381
+Distance between C-atom of backbone and the dipole at peptide bond 5: 0.868000
+Transition Dipole peptide bond 5: 0.037926 -0.254789 -0.281348, Length: 0.381461 and CO vector and length: (-0.622009 0.854004 0.625999, 1.228045)
+
+ x,y,z & length of dipole 6:  0.342 -0.057 -0.134  0.371
+Distance between C-atom of backbone and the dipole at peptide bond 6: 0.868000
+Transition Dipole peptide bond 6: 0.341585 -0.056926 -0.133761, Length: 0.371232 and CO vector and length: (-0.972000 0.735992 0.155998, 1.229148)
+
+ x,y,z & length of dipole 7:  0.131 -0.261  0.227  0.370
+Distance between C-atom of backbone and the dipole at peptide bond 7: 0.868000
+Transition Dipole peptide bond 7: 0.130840 -0.260933 0.227378, Length: 0.370008 and CO vector and length: (-0.209991 1.186005 -0.243999, 1.228918)
+
+
+
+Dipoles:  2   1 Difference vector:   nan   nan   nan
+Difference vector length calc. 1:   nan
+Dipole length:  2 = 449.049,  1 =   nan
+Difference vector length calc. 2:   nan
+Dipoles:  3   1 Difference vector:   nan   nan   nan
+Difference vector length calc. 1:   nan
+Dipole length:  3 = 446.620,  1 =   nan
+Difference vector length calc. 2:   nan
+Dipoles:  3   2 Difference vector: -0.656 -2.153 2.058
+Difference vector length calc. 1: 3.050
+Dipole length:  3 = 446.620,  2 = 449.049
+Difference vector length calc. 2: 3.050
+Dipoles:  4   1 Difference vector:   nan   nan   nan
+Difference vector length calc. 1:   nan
+Dipole length:  4 = 444.188,  1 =   nan
+Difference vector length calc. 2:   nan
+Dipoles:  4   2 Difference vector: -1.880 -4.549 -0.208
+Difference vector length calc. 1: 4.926
+Dipole length:  4 = 444.188,  2 = 449.049
+Difference vector length calc. 2: 4.926
+Dipoles:  4   3 Difference vector: -1.224 -2.396 -2.267
+Difference vector length calc. 1: 3.518
+Dipole length:  4 = 444.188,  3 = 446.620
+Difference vector length calc. 2: 3.518
+Dipoles:  5   1 Difference vector:   nan   nan   nan
+Difference vector length calc. 1:   nan
+Dipole length:  5 = 446.020,  1 =   nan
+Difference vector length calc. 2:   nan
+Dipoles:  5   2 Difference vector: 1.321 -4.274 -1.893
+Difference vector length calc. 1: 4.857
+Dipole length:  5 = 446.020,  2 = 449.049
+Difference vector length calc. 2: 4.857
+Dipoles:  5   3 Difference vector: 1.977 -2.121 -3.951
+Difference vector length calc. 1: 4.901
+Dipole length:  5 = 446.020,  3 = 446.620
+Difference vector length calc. 2: 4.901
+Dipoles:  5   4 Difference vector: 3.201 0.275 -1.684
+Difference vector length calc. 1: 3.628
+Dipole length:  5 = 446.020,  4 = 444.188
+'''
 
